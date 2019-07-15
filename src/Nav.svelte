fce31222af62ccf936cf3e6f491114498a2a3d8f
Matt Henderson <matt_henderson@sil.org>
2019-07-15 16:21:51 -0400

    Further refine nav menu items

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -1,4 +1,5 @@
 <script>
+  export let current = 'requests';
 </script>
 
 <style>
@@ -12,16 +13,17 @@
 
    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
+         <li class="nav-item">
+           <a class="nav-link" href="#requests">Requests</a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link" href="#commitments">Commitments</a>
+         </li>
          <li class="nav-item active">
-         <a class="nav-link" href="#messages">Messages <span class="sr-only">(current)</span></a>
+           <a class="nav-link" href="#messages">Messages <span class="sr-only">(current)</span></a>
          </li>
-         <li class="nav-item">
-         <a class="nav-link" href="#commitments">Commitments</a>
-         </li>
-         <li class="nav-item">
-         <a class="nav-link" href="#requests">
-            Requests
-         </a>
+         <li>
+           <a class="nav-link" href="#profile">(avatar)</a>
          </li>
       </ul>
    </div>
