007a0454048d0bb4fb473da78bae3858c337b548
Matt Henderson <matt_henderson@sil.org>
2019-07-16 11:09:59 -0400

    Add dummy content for Profile page

--- src/Profile.svelte
+++ src/Profile.svelte
@@ -2,6 +2,52 @@
 </script>
 
 <style>
+h3 {
+    font-size: 1.4rem;
+}
 </style>
 
-<h2>Profile</h2>
+<div class="row">
+    <div class="col">
+        <h2>John Smith</h2>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-4 col-lg-3">
+        <img src="https://avatars3.githubusercontent.com/u/6233204?s=180&v=4"
+             style="width: 100%; max-width: 100px; height: auto;"/>
+    </div>
+    <div class="col-sm-8 col-lg-9">
+        <h3>Contact Info</h3>
+        <div class="container-fluid">
+            <dl class="row">
+                <dt class="col-sm-4 col-lg-3 col-xl-2">Email:</dt>
+                <dd class="col-sm-8 col-lg-9 col-xl-10">john_smith@example.org</dd>
+
+                <dt class="col-sm-4 col-lg-3 col-xl-2">WhatsApp:</dt>
+                <dd class="col-sm-8 col-lg-9 col-xl-10">555-123-4567</dd>
+
+                <dt class="col-sm-4 col-lg-3 col-xl-2">Skype:</dt>
+                <dd class="col-sm-8 col-lg-9 col-xl-10">john_smith_example</dd>
+            </dl>
+        </div>
+
+        <h3>Preferences</h3>
+        <div class="container-fluid">
+            <dl class="row">
+                <dt class="col-sm-4 col-lg-3 col-xl-2">Language:</dt>
+                <dd class="col-sm-8 col-lg-9 col-xl-10">English (US)</dd>
+
+                <dt class="col-sm-4 col-lg-3 col-xl-2">Weight:</dt>
+                <dd class="col-sm-8 col-lg-9 col-xl-10">kg</dd>
+
+                <dt class="col-sm-4 col-lg-3 col-xl-2">Dimensions:</dt>
+                <dd class="col-sm-8 col-lg-9 col-xl-10">cm/m/km</dd>
+
+                <dt class="col-sm-4 col-lg-3 col-xl-2">Currency:</dt>
+                <dd class="col-sm-8 col-lg-9 col-xl-10">USD ($)</dd>
+            </dl>
+        </div>
+    </div>
+</div>
