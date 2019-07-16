0381f31b79fed7a10b2911ea93377e763a6350cd
Kate Brenner <kate_maher_brenner@sil.org>
2019-07-16 15:46:10 -0400

    first working version of basic data table Requests

---
+++ src/RequestForm.svelte
@@ -0,0 +1,82 @@
+<script>
+</script>
+
+<style>
+h3 {
+    font-size: 1.4rem;
+}
+</style>
+
+<div class="row">
+    <div class="col">
+        <h2>Add a Request</h2>
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Need By</label>
+                    <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="7/16/2019">
+                </div>
+                    <div class="form-group">
+                                <label for="need_after">Need After</label>
+                                <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                              </div>
+
+
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Destination</label>
+            <input type="text" id="exampleFormControlInput1" placeholder="name@example.com">
+          </div>
+
+        <label for="max-cost" class="col-2 col-form-label">Max Cost</label>
+          <div class="col-10">
+            <input class="form-control" type="number" value="100" id="max-cost">
+          </div>
+
+<div class="form-group">
+<label class="mr-sm-2" for="inlineFormCustomSelect">Max Size</label>
+  <select class="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
+    <option selected>Choose...</option>
+    <option value="1">Small (purse)</option>
+    <option value="2">Medium (laptop)</option>
+    <option value="3">Large (suitcase)</option>
+  </select>
+</div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect2">Categories</label>
+            <select multiple class="form-control" id="exampleFormControlSelect2">
+              <option>Technology</option>
+              <option>Food</option>
+              <option>Personal</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        </form>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Request</th>
+                    <th>Destination</th>
+                    <th>Needed After</th>
+                    <th>Needed Before</th>
+                </tr>
+             </thead>
+             <tbody>
+                <tr>
+                    <td>Burt's Bee's Lip Balm</td>
+                    <td>Atlanta, GA, USA</td>
+                    <td>18 Jul </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Jif Peanut Butter</td>
+                    <td>JAARS, NC, USA</td>
+                    <td></td>
+                    <td>3 Aug</td>
+                 </tr>
+             </tbody>
+        </table>
+    </div>
+
+</div>
--- src/Requests.svelte
+++ src/Requests.svelte
@@ -1,7 +1,58 @@
 <script>
+    jQuery(document).ready(function() {
+        jQuery('#allrequests').DataTable();
+    } );
 </script>
 
 <style>
+h3 {
+    font-size: 1.4rem;
+}
 </style>
 
-<h2>Requests</h2>
+<div class="row">
+    <h2>Requests</h2>
+</div>
+
+<div class="row">
+
+    <div class="col-12">
+
+
+
+        <table class="table table-hover" id="allrequests">
+            <thead>
+
+                <tr>
+                    <th>Request</th>
+                    <th>Destination</th>
+                    <th>Needed After</th>
+                    <th>Needed Before</th>
+                    <th>Cost</th>
+                    <th>Category</th>
+                    <th>Size</th>
+                </tr>
+             </thead>
+             <tbody>
+                <tr>
+                    <td>Burt's Bee's Lip Balm</td>
+                    <td>Atlanta, GA, USA</td>
+                    <td>18 Jul </td>
+                    <td></td>
+                    <td>4</td>
+                    <td>Personal</td>
+                    <td>Tiny</td>
+                </tr>
+                <tr>
+                    <td>Jif Peanut Butter</td>
+                    <td>JAARS, NC, USA</td>
+                    <td></td>
+                    <td>3 Aug</td>
+                    <td>5</td>
+                    <td>Food</td>
+                    <td>Small</td>
+                 </tr>
+             </tbody>
+        </table>
+    </div>
+</div>
