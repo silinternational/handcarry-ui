5dfb7ab2acb410cc91a25adbb859c4e23eb25518
Matt Henderson <matt_henderson@sil.org>
2019-07-16 14:16:05 -0400

    Add Messages markup (with example content)

--- src/Messages.svelte
+++ src/Messages.svelte
@@ -4,4 +4,51 @@
 <style>
 </style>
 
-<h2>Messages</h2>
+<div class="row">
+    <div class="col">
+        <h2>Messages</h2>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-5 col-lg-4">
+        <div class="list-group" id="myList" role="tablist">
+            <a class="list-group-item list-group-item-action active" data-toggle="list" href="#messages17" role="tab">
+                Peanut Butter - Clark Kent
+            </a>
+            <a class="list-group-item list-group-item-action" data-toggle="list" href="#messages21" role="tab">
+                Altoids - Jane Doe
+            </a>
+        </div>
+    </div>
+    <div class="col-sm-7 col-lg-8">
+        <div class="tab-content">
+            <div class="tab-pane active" id="messages17" role="tabpanel">
+                <h3>Peanut Butter - Clark Kent</h3>
+
+                <blockquote class="blockquote text-right">
+                  <p class="mb-0">Any chance you can bring some PB?</p>
+                  <footer class="blockquote-footer">you</footer>
+                </blockquote>
+
+                <blockquote class="blockquote">
+                  <p class="mb-0">Absolutely!</p>
+                  <footer class="blockquote-footer">Clark Kent</footer>
+                </blockquote>
+            </div>
+            <div class="tab-pane" id="messages21" role="tabpanel">
+                <h3>Peanut Butter - Clark Kent</h3>
+
+                <blockquote class="blockquote">
+                  <p class="mb-0">Did you find any Wintergreen Altoids?</p>
+                  <footer class="blockquote-footer">Jane Doe</footer>
+                </blockquote>
+
+                <blockquote class="blockquote text-right">
+                  <p class="mb-0">No luck, sorry</p>
+                  <footer class="blockquote-footer">you</footer>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+</div>
