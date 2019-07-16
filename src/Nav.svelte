52e1c9bb585bff848a7b50462d57a760c5f5808b
Billy Clark <billy_clark@sil.org>
2019-07-16 13:52:51 -0400

    prep for authn handshake

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -6,7 +6,7 @@
 </style>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-2">
-   <a class="navbar-brand" href="#">HandCarry</a>
+   <a class="navbar-brand" href="/">HandCarry</a>
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
    </button>
--- src/api.js
+++ src/api.js
@@ -1,35 +1,47 @@
+import './token.js'
+import token from './token.js';
+
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Supplying_request_options
-export async function get(url) {
-  const res = await fetch('https://httpbin.org/get')
-  return await res.json()
+export async function get() {
+  return await f('https://httpbin.org/get')
 }
 
 export async function gql() {
-  const q = `{
-    getLocation(ip: "8.8.8.8") {
-      country {
-          names {
-            en
-          }
-          geoname_id
-          iso_code
-        }
-        location {
-          latitude
-          longitude
-        }
-      }
-    }`
-  
-  const res = await fetch("https://api.graphloc.com/graphql", {
+  return await f('https://api.graphloc.com/graphql', {
     method: 'post',
-    headers: {
-      'content-type': 'application/json'
-    },
     body: JSON.stringify({
-      query: q
+      query: query.geoLocation('8.8.8.8')
     }),
   })
+}
 
-  return await res.json()
+const headers = {
+  'content-type': 'application/json',
+  Authorization: token.authzHeader(),
+}
+
+async function f(url, config = {}) {
+  config.headers = headers
+  
+  const response = await fetch(url, config)
+  // TODO: check response is ok before assuming anything (https://developer.mozilla.org/en-US/docs/Web/API/Response/ok)
+  return response.json()
+}
+
+const query = {
+  geoLocation: ip => `{
+    getLocation(ip: "${ip}") {
+      country {
+        names {
+          en
+        }
+        geoname_id
+        iso_code
+      }
+      location {
+        latitude
+        longitude
+      }
+    }
+  }`
 }
