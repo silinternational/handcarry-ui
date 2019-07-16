f74bff3e9a9c2d45b75e74b4fd840e27572b2b0f
Matt Henderson <matt_henderson@sil.org>
2019-07-16 16:04:03 -0400

    Add friendly date/time to message display

--- src/Messages.svelte
+++ src/Messages.svelte
@@ -1,4 +1,7 @@
 <script>
+function whenWas(timestamp) {
+    return moment(timestamp).fromNow();
+}
 </script>
 
 <style>
@@ -33,12 +36,12 @@
 
                 <blockquote class="blockquote text-right">
                   <p class="mb-0">Any chance you can bring some PB?</p>
-                  <footer class="blockquote-footer">you</footer>
+                  <footer class="blockquote-footer">you, { whenWas(1563298677000) }</footer>
                 </blockquote>
 
                 <blockquote class="blockquote">
                   <p class="mb-0">Absolutely!</p>
-                  <footer class="blockquote-footer">Clark Kent</footer>
+                  <footer class="blockquote-footer">Clark Kent, { whenWas(1563307287000) }</footer>
                 </blockquote>
             </div>
             <div class="tab-pane card-body" id="messages21" role="tabpanel">
@@ -47,12 +50,12 @@
 
                 <blockquote class="blockquote">
                   <p class="mb-0">Did you find any Wintergreen Altoids?</p>
-                  <footer class="blockquote-footer">Jane Doe</footer>
+                  <footer class="blockquote-footer">Jane Doe, { whenWas(1563288647000) }</footer>
                 </blockquote>
 
                 <blockquote class="blockquote text-right">
                   <p class="mb-0">No luck, sorry</p>
-                  <footer class="blockquote-footer">you</footer>
+                  <footer class="blockquote-footer">you, { whenWas(1563288648000) }</footer>
                 </blockquote>
             </div>
         </div>
