0751d96acb2531174b85dd469b542f78894c127d
Matt Henderson <matt_henderson@sil.org>
2019-07-16 11:45:44 -0400

    Support dynamic profile details (still using dummy defaults)

--- src/Profile.svelte
+++ src/Profile.svelte
@@ -1,4 +1,11 @@
 <script>
+export let name = 'John Smith';
+export let avatarUrl = 'https://avatars3.githubusercontent.com/u/6233204?s=180&v=4';
+export let contactMethods = [
+    { 'type': 'Email', 'value': 'john_smith@example.org' },
+    { 'type': 'WhatsApp', 'value': '555-123-4567' },
+    { 'type': 'Skype', 'value': 'john_smith_example' },
+];
 </script>
 
 <style>
@@ -9,27 +16,23 @@ h3 {
 
 <div class="row">
     <div class="col">
-        <h2>John Smith</h2>
+        <h2>{name}</h2>
     </div>
 </div>
 
 <div class="row">
     <div class="col-sm-4 col-lg-3">
-        <img src="https://avatars3.githubusercontent.com/u/6233204?s=180&v=4"
+        <img src="{avatarUrl}"
              style="width: 100%; max-width: 100px; height: auto;"/>
     </div>
     <div class="col-sm-8 col-lg-9">
         <h3>Contact Info</h3>
         <div class="container-fluid">
             <dl class="row">
-                <dt class="col-sm-4 col-lg-3 col-xl-2">Email:</dt>
-                <dd class="col-sm-8 col-lg-9 col-xl-10">john_smith@example.org</dd>
-
-                <dt class="col-sm-4 col-lg-3 col-xl-2">WhatsApp:</dt>
-                <dd class="col-sm-8 col-lg-9 col-xl-10">555-123-4567</dd>
-
-                <dt class="col-sm-4 col-lg-3 col-xl-2">Skype:</dt>
-                <dd class="col-sm-8 col-lg-9 col-xl-10">john_smith_example</dd>
+                {#each contactMethods as contactMethod}
+                    <dt class="col-sm-4 col-lg-3 col-xl-2">{contactMethod.type}:</dt>
+                    <dd class="col-sm-8 col-lg-9 col-xl-10">{contactMethod.value}</dd>
+                {/each}
             </dl>
         </div>
 
