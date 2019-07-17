3a85ab1bb89fc7890d16e77e0966275cce6d277c
Matt Henderson <matt_henderson@sil.org>
2019-07-17 09:31:05 -0400

    Add dropdown entries back to Requests menu item

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -13,11 +13,16 @@ import {location} from 'svelte-spa-router' // https://github.com/ItalyPaleAle/sv
 
    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
-         <li class="nav-item">
-            <a class="nav-link" class:active="{$location === '/requests'}" href="#/requests">
+         <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" class:active="{ $location.startsWith('/request') }"
+               href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Requests
-             {#if $location === '/requests'}<span class="sr-only">(current)</span>{/if}
-           </a>
+              {#if $location === '/requests'}<span class="sr-only">(current)</span>{/if}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+               <a class="dropdown-item" href="#/requests">View Requests</a>
+               <a class="dropdown-item" href="#/requestform">Make a Request</a>
+            </div>
          </li>
          <li class="nav-item">
             <a class="nav-link" class:active="{$location === '/commitments'}" href="#/commitments">
