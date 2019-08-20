f54a93dfbbfcd4c5088a0fbe155c4824e18b2ceb
Billy Clark <billy_clark@sil.org>
2019-08-20 14:39:03 -0400

    button was too small

--- src/Login.svelte
+++ src/Login.svelte
@@ -26,7 +26,7 @@ async function loadUserInfo () {
   <div class="form-group row align-items-center">
     <!-- svelte-ignore a11y-autofocus -->
     <input id="email" class="form-control form-control-lg col-4 mr-4" type="email" bind:value={ email } placeholder="Enter email address" autofocus>
-    <button type="submit" class="btn btn-primary btn-l">Sign in</button>
+    <button type="submit" class="btn btn-primary btn-lg">Sign in</button>
   </div>
 </form>
 
