52b514475c990f14191c3d9ce57d1c58932b9a45
Matt Henderson <matt_henderson@sil.org>
2019-09-24 10:48:16 -0400

    Give request-info page a better designed layout

--- src/views/RequestInfo.svelte
+++ src/views/RequestInfo.svelte
@@ -1,17 +1,39 @@
 <script>
+import CategoryIcon from '../components/CategoryIcon.svelte'
+import SizeIcon from '../components/SizeIcon.svelte'
 import { getRequest, cancelPost } from '../data/gqlQueries'
 import { push } from 'svelte-spa-router'
 
 export let params = {} // URL path parameters, provided by router.
 
 let request = {}; loadRequest()
+let category = ''
+let cost = ''
+let description = ''
+let destination = ''
+let origin = ''
 let requestor = ''
+let requestorNickname = ''
+let size = ''
+let status = ''
+let title = ''
+let url = ''
 let me = {}
 
 async function loadRequest() {
   const response = await getRequest(params.id)
   request = response.post
+  category = request.category
+  cost = request.cost || ''
+  description = request.description
+  destination = request.destination
+  origin = request.origin
   requestor = request.createdBy
+  requestorNickname = requestor.nickname
+  size = request.size
+  status = request.status
+  title = request.title
+  url = request.url
   me = response.user
 }
 
@@ -26,21 +48,103 @@ async function cancel() {
     // TODO: need errorhandling
   }
 }
+
+function asReadableDate(timestamp) {
+  return new Date(timestamp).toLocaleDateString()
+}
 </script>
 
-<h2 class="pb-4">{ request.title }</h2>
+<h2 class="text-center m-4">{ title }</h2>
 
-<div class="alert alert-warning text-center" role="alert">
-  Kate, make me beautiful please
+<div class="row">
+  <div class="col">
+    <blockquote class="blockquote">
+      <p class="mb-0">{ description }</p>
+      <footer class="blockquote-footer">{ requestorNickname }</footer>
+    </blockquote>
+    {#if url }
+      <p>{ url }</p>
+    {/if}
+  </div>
+  
+  <div class="col-3 col-sm-2">
+    <div class="row">
+      <div class="col">
+        {#if category }
+          <p class="text-center">
+            <CategoryIcon {category} />
+            <small><i>{ category }</i></small>
+          </p>
+        {/if}
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col">
+        {#if size }
+          <p class="text-center">
+            <SizeIcon {size} />
+            <small><i>{ size }</i></small>
+          </p>
+        {/if}
+      </div>
+    </div>
+  </div>
 </div>
 
-<pre style="font-size: x-small;">{ JSON.stringify(request, null, 2)}</pre>
+<hr />
 
-<a href="#/requests" class="btn btn-secondary" role="button">Back to requests</a>
-{#if isMine()}
-<button on:click={cancel} class="btn btn-danger">cancel this request</button>
-{:else}
-<a href={`${window.location.hash}/new-conversation?with=${requestor.nickname}`} class="btn btn-success btn-sm" role="button">
-  Start a conversation with { requestor.nickname } about this item
-</a>
-{/if}
+<div class="row">
+  <div class="col-md">
+    {#if destination }
+      <p>
+        <b>Destination:</b><br />
+        { destination }
+      </p>
+    {/if}
+      
+    {#if origin }
+      <p>
+        <b>Origin:</b><br />
+        { origin }
+      </p>
+    {/if}
+  </div>
+  
+  <div class="col-md">
+    <p>
+      {#if category }
+        <b>Category:</b> { category }<br />
+      {/if}
+      
+      {#if size }
+        <b>Size:</b> { size }<br />
+      {/if}
+      
+      {#if cost }
+        <b>Cost:</b> { cost }<br />
+      {/if}
+    </p>
+  </div>
+  
+  <div class="col-md">
+    <p>
+      <b>Status:</b> { status }<br />
+    </p>
+  </div>
+</div>
+
+<hr />
+
+<div class="row">
+  <div class="col text-center">
+    <a href="#/requests" class="btn btn-secondary btn-sm m-1" role="button">Back to requests</a>
+    {#if isMine()}
+      <button on:click={cancel} class="btn btn-danger btn-sm m-1">Cancel this request</button>
+    {:else}
+      <a href={`${window.location.hash}/new-conversation?with=${requestor.nickname}`} class="btn btn-success btn-sm m-1" role="button">
+        Chat with { requestor.nickname } about this
+      </a>
+    {/if}
+  </div>
+</div>
