3a9dc1599caa3c480123985cd29beb63bcd2faf4
Matt Henderson <matt_henderson@sil.org>
2019-09-25 10:37:03 -0400

    Give the initial "Sign in" form a more central, responsive placement

--- src/views/Login.svelte
+++ src/views/Login.svelte
@@ -8,12 +8,15 @@ let email = ''
 <style>
 </style>
 
-<h1 class="pb-4">Sign in</h1>
-
 <form on:submit|preventDefault={ () => login(email, '/requests') }>
-  <div class="form-group row align-items-center">
+  <div class="form-row">
+    <div class="col-md-6 offset-md-2 col-lg-5 offset-lg-3">
+      <h1 class="pt-5 pb-4">Sign in</h1>
+    </div>
+  </div>
+  <div class="form-group form-row align-items-center">
     <!-- svelte-ignore a11y-autofocus -->
-    <input id="email" class="form-control form-control-lg col-4 mr-4" type="email" bind:value={ email } placeholder="Enter email address" autofocus>
-    <button type="submit" class="btn btn-primary btn-lg">Sign in</button>
+    <input id="email" class="form-control form-control-lg col-md-6 offset-md-2 col-lg-5 offset-lg-3 mr-4 my-2" type="email" bind:value={ email } placeholder="Enter email address" autofocus>
+    <button type="submit" class="btn btn-primary btn-lg my-2">Sign in</button>
   </div>
 </form>
