d07d49a273e6ff43a60c617e47024b736d0fdd42
Billy Clark <billy_clark@sil.org>
2019-10-07 15:49:04 -0400

    a11y complaining

--- src/components/SizeSelector.svelte
+++ src/components/SizeSelector.svelte
@@ -43,7 +43,7 @@ label {
         <input type="radio" name="size" id="size{size.name}" value={size.name} class="form-check-input" bind:group={selectedName}>
         <div class="btn btn-block h-100 d-flex align-items-end flex-column" class:btn-primary={selectedName === size.name} class:btn-outline-dark={selectedName !== size.name}>
           <div class="description w-100 d-sm-inline">{size.description}</div>
-          <div class="picture mt-auto w-100 d-sm-inline"><img src="{size.pictureUrl}" style="width: 80px;" /></div>
+          <div class="picture mt-auto w-100 d-sm-inline"><img src="{size.pictureUrl}" style="width: 80px;" alt={`picture of something ${size.name}`} /></div>
           <div class="title w-100 d-sm-inline"><SizeIndicator size={size.name} /></div>
         </div>
       </label>
