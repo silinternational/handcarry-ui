f460cfc2a97fe6cc4ec740b1eaaf278403f85e9f
Matt Henderson <matt_henderson@sil.org>
2019-10-08 08:23:51 -0400

    Add a little left-padding to the login "Email" field

--- src/views/Login.svelte
+++ src/views/Login.svelte
@@ -16,7 +16,7 @@ let email = ''
   </div>
   <div class="form-group form-row align-items-center">
     <!-- svelte-ignore a11y-autofocus -->
-    <input id="email" class="form-control form-control-lg col-md-6 offset-md-2 col-lg-5 offset-lg-3 mr-4 my-2" type="email" bind:value={ email } placeholder="Enter email address" autofocus>
+    <input id="email" class="form-control form-control-lg col-md-6 offset-md-2 col-lg-5 offset-lg-3 mr-4 my-2 pl-2" type="email" bind:value={ email } placeholder="Enter email address" autofocus>
     <button type="submit" class="btn btn-primary btn-lg my-2">Sign in</button>
   </div>
 </form>
