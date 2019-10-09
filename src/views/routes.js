17fe44ee8646a37df118a18da0dc7500eb641e4a
Billy Clark <billy_clark@sil.org>
2019-10-09 10:31:19 -0400

    name wasn't consistent

--- src/views/routes.js
+++ src/views/routes.js
@@ -2,7 +2,7 @@ import Messages from './Messages.svelte'
 import Profile from './Profile.svelte'
 import NewRequest from './NewRequest.svelte'
 import Requests from './Requests.svelte'
-import RequestInfo from './RequestInfo.svelte'
+import Request from './Request.svelte'
 import Login from './Login.svelte'
 import Welcome from './Welcome.svelte'
 import UploadPlayground from './UploadPlayground.svelte'
@@ -14,7 +14,7 @@ const routes = {
 	'/welcome': Welcome,
 	'/requests': Requests,
 	'/requests/new': NewRequest,
-	'/requests/:id': RequestInfo,
+	'/requests/:id': Request,
 	'/messages': Messages,
 	'/messages/:id': Messages,
 	'/messages/new-conversation/:postId': Messages,
