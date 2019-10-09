e02a0d1e92de75f378c1535b0e31010ce8332be6
Matt Henderson <matt_henderson@sil.org>
2019-10-09 16:30:16 -0400

    Adjust "luggage" size filters, show size name too

--- src/components/SizeFilter.svelte
+++ src/components/SizeFilter.svelte
@@ -39,22 +39,40 @@ function getTextColorFor(size, selectedSizeType) {
 }
 
 .badge {
-  vertical-align: top;
+  vertical-align: middle;
 }
 
 label {
   cursor: pointer;
   margin: 1px;
 }
+
+label:hover .name {
+  opacity: 100;
+}
+
+.name {
+  height: 0;
+  opacity: 0;
+  position: absolute;
+  top: 1.5em;
+  z-index: 10;
+}
+.name > .badge {
+  font-size: 0.75rem;
+}
 </style>
 
-<div class="text-center">
+<div class="d-inline-block text-center text-nowrap">
   {#each sizes as size }
     <div class="d-inline-block" style="font-size: {heights[size.type]}">
-      <label class="form-check-label d-block" for="size{size.type}" title="{size.name}">
+      <label class="form-check-label d-block position-relative" for="size{size.type}" title="{size.name}">
         <div class="badge badge-{getBadgeColorFor(size, selectedSizeType)}">
           <input type="radio" name="size" id="size{size.type}" value={size.type} class="form-check-input" bind:group={selectedSizeType}>
           <svg class="lnr lnr-briefcase text-{getTextColorFor(size, selectedSizeType)}"><use xlink:href="#lnr-briefcase"></use></svg>
+        </div>
+        <div class="name text-center">
+          <div class="d-block badge badge-{size.color}">{size.name}</div>
         </div>
       </label>
     </div>
