15d5a604fe6a206953692e1949bd4a4fded4dfb3
Billy Clark <billy_clark@sil.org>
2019-10-09 13:42:45 -0400

    found a common relationship between images and post sizes

--- src/components/SizeIndicator.svelte
+++ src/components/SizeIndicator.svelte
@@ -3,7 +3,9 @@ import { sizes } from '../data/sizes'
 
 export let size
 
-$: color = sizes.find(s => s.type === size).color
+$: match = sizes.find(s => s.type === size) || {}
+$: color = match.color || 'secondary'
+$: name = match.name || 'unknown'
 </script>
 
-<span class="badge badge-pill badge-{color}">{ size.toLowerCase() }</span>
+<span class="badge badge-pill badge-{color}">{ name }</span>
