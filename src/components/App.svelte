44e2f1193467824dee57c57f9fae4de941d589ba
Billy Clark <billy_clark@sil.org>
2019-11-18 11:29:17 -0500

    welcome page wasn't built out yet

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,6 +1,7 @@
 <script>
 import Nav from './Nav.svelte'
 import Router from 'svelte-spa-router' // https://github.com/ItalyPaleAle/svelte-spa-router
+import { location } from 'svelte-spa-router'
 import Footer from './Footer.svelte'
 import routes from '../views/routes'
 import Bootstrap from '../components/Bootstrap.svelte'
@@ -8,7 +9,9 @@ import Bootstrap from '../components/Bootstrap.svelte'
 
 <Bootstrap />
 
-<Nav />
+{#if $location !== '/welcome'}
+  <Nav />
+{/if}
 
 <main class="container">
   <Router {routes} />
--- src/views/Welcome.svelte
+++ src/views/Welcome.svelte
@@ -1,3 +1,33 @@
-<h1 class="pb-4">Welcome</h1>
+<div class="d-flex flex-column align-items-center">
+  <h1 class="py-4">Welcome to WeCarry!</h1>
 
-<a href="#/requests">current requests</a>
+  <!-- TODO: get real video from Sheryl -->
+  <iframe src="https://www.youtube.com/embed/kXDeNWDlEqk" frameborder="0" title="Welcome video"/>
+  
+  <a href="/welcome/preferences" class="btn btn-primary mt-4">Continue</a>
+
+  <footer class="pt-4">
+    <a href="/welcome/privacy" class="pr-4">
+      <small class="text-warning">Privacy policy</small>
+    </a>
+
+    <a href="/welcome/terms">
+      <small class="text-warning">Terms of use</small>
+    </a>
+  </footer>
+</div>
+
+<style>
+iframe {
+  height: 315px;
+  width: 560px;
+}
+
+/* ensures the entire video remains visible on phones */
+@media (max-width: 576px) { 
+  iframe {
+    height: auto;
+    width: 95vw;
+  }
+}
+</style>
