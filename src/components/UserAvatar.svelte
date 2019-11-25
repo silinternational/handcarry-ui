d054fbd8367dcc47726c0795f1dae52a512016e4
Billy Clark <billy_clark@sil.org>
2019-11-25 09:51:46 -0500

    learned that `photoURL` will always be populated

--- src/components/UserAvatar.svelte
+++ src/components/UserAvatar.svelte
@@ -2,24 +2,13 @@
 export let user
 
 $: nickname = user.nickname || ''
-$: initial = Array.from(nickname)[0] || ''
 </script>
 
 <style>
-div {
-  width: 1.6em;
-  height: 1.6em;
-}
 img {
   max-width: 100%;
   max-height: 100%;
 }
 </style>
 
-<div class="d-inline-block">
-  {#if user.photoURL }
-    <img class="rounded-circle border text-center" src={user.photoURL} alt={nickname} title={nickname} />
-  {:else}
-    <span class="badge badge-pill badge-primary" title={nickname}>{initial}</span>
-  {/if}
-</div>
+<img class="rounded-circle border text-center" src={user.photoURL} alt={nickname} title={nickname} />
