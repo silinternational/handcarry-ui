2545bb23340c24a7670acbc9d5a3d32267e24e6d
Billy Clark <billy_clark@sil.org>
2019-11-25 11:35:48 -0500

    backend api will change alllowing ui to determine default images

--- src/components/UserAvatar.svelte
+++ src/components/UserAvatar.svelte
@@ -1,7 +1,11 @@
 <script>
+import md5 from 'blueimp-md5'
+
 export let user
 
 $: nickname = user.nickname || ''
+$: hashedEmail = md5((user.email || '').trim().toLowerCase()) // https://en.gravatar.com/site/implement/hash/
+$: src = user.photoURL || `https://www.gravatar.com/avatar/${hashedEmail}?default=mp` // https://en.gravatar.com/site/implement/images#default-image
 </script>
 
 <style>
@@ -11,4 +15,4 @@ img {
 }
 </style>
 
-<img class="rounded-circle border text-center" src={user.photoURL} alt={nickname} title={nickname} />
+<img class="rounded-circle border" {src} alt={nickname} title={nickname} />
