b3384e269b356749a53df2e57de8b6cd5f6dc950
Matt Henderson <matt_henderson@sil.org>
2019-12-18 12:03:48 -0500

    Move "sticky footer" CSS into the Footer component

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,22 @@
+<style global>
+/* Sticky footer styles */
+html {
+    position: relative;
+    min-height: 100%;
+}
+html > body {
+    margin-bottom: 70px; /* >= fixed height of footer. */
+}
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 40px; /* fixed height of footer */
+    line-height: 40px; /* = fixed height of footer (to vertically center the text) */
+}
+/* End of sticky footer styles */
+</style>
+
 <footer class="footer text-center small">
     <a href="#/welcome/terms" class="mx-2 text-muted">Terms of Use</a>
     |
