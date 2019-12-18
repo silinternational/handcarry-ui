b4c8c9ef9494c7692ef3898b542b5a2082d78d98
Matt Henderson <matt_henderson@sil.org>
2019-12-18 11:55:00 -0500

    Add terms/privacy links to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,2 +1,5 @@
-<footer class="mt-4">
+<footer class="footer text-center small">
+    <a href="#/welcome/terms" class="mx-2 text-muted">Terms of Use</a>
+    |
+    <a href="#/welcome/privacy" class="mx-2 text-muted">Privacy Policy</a>
 </footer>
