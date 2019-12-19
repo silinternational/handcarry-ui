7c2940c483059b595df3a16cdfac2883974783c4
Matt Henderson <matt_henderson@sil.org>
2019-12-19 16:09:23 -0500

    Prevent button-styling from affecting non-button SizeTile

--- src/components/SizeTile.svelte
+++ src/components/SizeTile.svelte
@@ -4,11 +4,16 @@ import SizeIndicator from '../components/SizeIndicator.svelte'
 
 export let size
 export let selected = false
+export let isButton = true
 
 $: match = sizes.find(s => s.type === size) || {}
 $: description = match.description || ''
 $: name = match.name || 'unknown'
 $: pictureUrl = match.pictureUrl || ''
+
+$: buttonCssClasses = 'btn btn-block ' + (selected ? 'btn-primary' : 'btn-outline-dark')
+$: nonButtonCssClasses = 'border p-2 rounded text-center'
+$: extraCssClasses = isButton ? buttonCssClasses : nonButtonCssClasses
 </script>
 
 <style>
@@ -32,7 +37,7 @@ img {
 }
 </style>
 
-<div class="btn btn-block h-100 d-flex align-items-end flex-column" class:btn-primary={selected} class:btn-outline-dark={!selected}>
+<div class="{extraCssClasses} h-100 d-flex align-items-end flex-column">
   <div class="description w-100 d-sm-inline pb-2">{description}</div>
   <div class="picture mt-auto w-100 d-sm-inline pb-2"><img src="{pictureUrl}" alt={`picture of something ${name}`} /></div>
   <div class="w-100 d-sm-inline"><SizeIndicator {size} /></div>
