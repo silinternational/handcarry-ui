194d8c7010a69254728e45841f48350febe8ae83
Matt Henderson <matt_henderson@sil.org>
2019-12-19 13:51:37 -0500

    Add privacy policy (with header links)

--- src/views/Privacy.svelte
+++ src/views/Privacy.svelte
@@ -1,9 +1,161 @@
-<h1 class="py-4">Privacy policy</h1>
+<script>
+import { tick } from 'svelte'
 
-<p>
-  policies go here
-</p>
+export let params
 
-<!-- TODO: buttons or links?  to go where? Take into account the welcome page does not have the nav bar 
-     and continuing may need to take advanatage of the `returnTo` url so the user ends up where they were 
-     originally heading. -->
+$: scrollToSection(params.section)
+
+async function scrollToSection(section) {
+  
+  // Wait for the DOM to be rendered.
+  await tick()
+  
+  const element = document.querySelector('#' + section)
+  element && element.scrollIntoView()
+}
+</script>
+
+<style>
+h2 {
+  margin-top: 36px;
+  margin-bottom: 18px;
+}
+</style>
+
+<h1 class="py-4">WeCarry Privacy Policy</h1>
+<p>Effective December 1, 2019</p>
+<p>Thank you for entrusting WeCarry with your hand-carry coordination needs. We understand this may include personal information about you such as your location and name, and so we take great care and responsibility to protect your information. This page describes what information we collect and how we use it.</p>
+
+<h2 id="the-short-version">The short version</h2>
+<p>We only collect the information you choose to give us, and we process it with your consent, or on another legal basis; we only require the minimum amount of personal information that is necessary to fulfill the purpose of your interaction with us; we don't sell it to third parties; and we only use it as this Privacy Policy describes. If you're visiting us from the European Union (EU), European Economic Area (EEA), Switzerland, or the United Kingdom (UK), please see our <a href="#/welcome/privacy/our-global-privacy-practices">global privacy practices</a>: we comply with the General Data Protection Regulation (GDPR). No matter where you are, where you live, or what your citizenship is, we provide a high standard of privacy protection to all our users around the world, regardless of their country of origin or location.</p>
+<p>Of course, the short version doesn't tell you everything, so please read on for more details!</p>
+
+<h2 id="outline">Outline</h2>
+<p><a href="#/welcome/privacy/the-short-version">The short version</a></p>
+<p><a href="#/welcome/privacy/outline">Outline</a></p>
+<p><a href="#/welcome/privacy/personal-information-wecarry-collects-and-why">Personal information WeCarry collects and why</a></p>
+<p><a href="#/welcome/privacy/how-we-share-the-information-we-collect">How we share the information we collect</a></p>
+<p><a href="#/welcome/privacy/how-you-can-access-or-request-removal-of-information-about-you">How you can access or request removal of information about you</a></p>
+<p><a href="#/welcome/privacy/how-we-use-cookies-and-tracking">How we use cookies and tracking</a></p>
+<p><a href="#/welcome/privacy/where-we-host-our-service-and-store-your-information">Where we host our service and store your information</a></p>
+<p><a href="#/welcome/privacy/how-we-secure-your-information">How we secure your information</a></p>
+<p><a href="#/welcome/privacy/our-global-privacy-practices">Our global privacy practices</a></p>
+<p><a href="#/welcome/privacy/how-we-communicate-with-you">How we communicate with you</a></p>
+<p><a href="#/welcome/privacy/changes-to-our-privacy-policy">Changes to our Privacy Policy</a></p>
+<p><a href="#/welcome/privacy/contacting-us">Contacting us</a></p>
+
+<h2 id="personal-information-wecarry-collects-and-why">Personal information WeCarry collects and why</h2>
+<p>We think very carefully about each piece of information we collect to be careful not to collect more than is necessary. This table outlines the information collected and how it is used.</p>
+<table class="table table-bordered">
+  <tbody>
+  <tr>
+    <th>Information</th>
+    <th>How it is used</th>
+  </tr>
+  <tr>
+    <td>Your name</td>
+    <td>Your name is made visible to your organization's administrator(s) so they know who is using the service. Your name is not made visible to any other users of the service (unless you or another user enters it as part of a message or request description, for example).
+    </td>
+  </tr>
+  <tr>
+    <td>Your email address</td>
+    <td>Your email address is used for system-generated communications and notifications, and a one-way hash of your email address is used to look up your Gravatar image. 
+    </td>
+  </tr>
+  <tr>
+    <td>Organization unique identifier</td>
+    <td>A computer-friendly unique identifier is shared with WeCarry from your authentication provider so that we can identify you during login. This is usually a big long number and does not contain personally identifiable information about you in it.
+    </td>
+  </tr>
+  <tr>
+    <td>Nickname</td>
+    <td>Rather than display your real name to others, we allow you to choose a nickname to be displayed. If you choose to use your real name for your nickname be aware others will see it.
+    </td>
+  </tr>
+  <tr>
+    <td>Avatar</td>
+    <td><p>By default we will use either an avatar (profile image) provided by your authentication provider or a <a href="https://gravatar.com" target="_blank">Gravatar</a> avatar if available. You may choose to change your avatar if you like. Your avatar is displayed with any requests you make or messages you send to others.</p></td>
+  </tr>
+  <tr>
+    <td>Language</td>
+    <td>We capture and store your browser's language preference so that we may present the WeCarry interface and communications to you in the most appropriate language.
+    </td>
+  </tr>
+  <tr>
+    <td>Timezone</td>
+    <td>We capture and store your timezone and display it to others during conversations so that they know what your current time is to manage their expectations of when you may be available to respond. 
+    </td>
+  </tr>
+  <tr>
+    <td>Default location (optional)</td>
+    <td>If you choose to set a default location for requests and notifications, we will store this. We do not display it to others unless you choose to use it in requests. 
+    </td>
+  </tr>
+  <tr>
+    <td>Unit of measurement (optional)</td>
+    <td>You may choose a default unit of measurement, for example kilograms or pounds, to be used when making requests or viewing other requests to better personalize the information for you.
+    </td>
+  </tr>
+  <tr>
+    <td>Request details</td>
+    <td>When creating requests you'll be able to provide additional request-specific information, and if you choose to include any information about yourself you will be disclosing it to others and WeCarry will not be aware of it. 
+    </td>
+  </tr>
+  </tbody>
+</table>
+
+<h2 id="how-we-share-the-information-we-collect">How we share the information we collect</h2>
+<p>We use external vendors for services such as helpdesk ticket management and log analysis. We do not share your information for any marketing or commercial purposes. Here is a list of vendors we use and what information is shared with them and why:</p>
+<table class="table table-bordered">
+  <tbody>
+  <tr>
+    <th>Vendor / Service</th>
+    <th>Information shared and why</th>
+  </tr>
+  <tr>
+    <td>Google Analytics</td>
+    <td>Anonymous tracking of WeCarry usage. Google Analytics aggregates traffic and usage information so that we can better understand at a high level how people use WeCarry and the features used. We do not share personally identifiable information with Google Analytics.</td>
+  </tr>
+  <tr>
+    <td>FreshDesk</td>
+    <td>FreshDesk is our helpdesk and knowledge base service provider. When you open a support or feedback ticket with us, you provide your email address and additional information as needed to describe your issue or request. At your request we can delete any records about you in FreshDesk.</td>
+  </tr>
+  <tr>
+    <td>Rollbar</td>
+    <td>Rollbar provides error log analysis services. We share with Rollbar your email address for identification of any errors you encounter. Errors reported to Rollbar are typically internal system errors, but it helps us to know who has experienced certain errors so that we can follow up with them when appropriate. Rollbar information is only retained for 30 days and is then purged. At your request we can delete any records about you in Rollbar.</td>
+  </tr>
+  <tr>
+    <td>Sendgrid</td>
+    <td>We use Sendgrid to send marketing and transactional emails. We also use Sendgrid to send SMS-based notifications. In these communications we may share your name, nickname, email address, and specific request information as needed for each specific communication.</td>
+  </tr>
+  </tbody>
+</table>
+
+<h2 id="how-you-can-access-or-request-removal-of-information-about-you">How you can access or request removal of information about you</h2>
+<p>You may send an email to support@wecarry.app to request access to the information we have about you or to request that we delete the information we have about you. In some cases where removal of your information is not possible due to impact to others or the service in general, your information will be anonymized to remove any connection with you. </p>
+
+<h2 id="how-we-use-cookies-and-tracking">How we use cookies and tracking</h2>
+<p>WeCarry uses Google Analytics to help us understand how visitors interact with the app, but we do NOT use it to track, collect or send to Google any data that personally identifies an individual. For more information on how Google Analytics collects and processes data, see "How Google uses data when you use our partners' sites or apps" <a href="https://www.google.com/policies/privacy/partners/" target="_blank">https://www.google.com/policies/privacy/partners/</a>.</p>
+<p>Other than any cookies used by Google Analytics (which we cannot see) and the session cookie automatically used by your browser to allow you to login (if applicable), WeCarry does not use cookies to collect any data about you.</p>
+<p>While we may provide links to other websites, please be aware that we are not responsible for the content or privacy practices of third-party websites.</p>
+
+<h2 id="where-we-host-our-service-and-store-your-information">Where we host our service and store your information</h2>
+<p>WeCarry is hosted on Amazon Web Services (AWS) in their US East region, specifically in Virginia, U.S. AWS provides highly secure and resilient services that meet many if not all industry standards and regulations. To learn more about AWS and their security practices and certifications visit <a href="https://aws.amazon.com" target="_blank">https://aws.amazon.com</a>. Each of the other vendors mentioned above operate and manage their own servers and services. More information about each can be found on their own websites. </p>
+
+<h2 id="how-we-secure-your-information">How we secure your information</h2>
+<p>We believe in a defense-in-depth strategy that leverages multiple layers of security to provide the best level of protection available. We utilize a web-application firewall that inspects all traffic for malicious intent as well as blocks known bad actors and emerging attacks. We use strict firewall rules that limit access to only traffic that has gone through the web-application firewall. Our servers are managed in what is called a "virtual private cloud" which means they exist on a private network where no other traffic is able to reach them. Further, your information stored in our database is encrypted both in transit and at rest. Finally, we operate with a least-access privilege model which restricts access to servers and services to only the individuals or other services which need access to them. All access into and out of the network is logged as well as administrative access to servers and services. We're not only the creators of WeCarry, we're users too. We trust the platform with our needs and we believe you can too. </p>
+
+<h2 id="our-global-privacy-practices">Our global privacy practices</h2>
+<p>We store and process the information that we collect in the United States in accordance with this Privacy Policy (our vendor services may store and process data outside the United States). However, we understand that we have users from different countries and regions with different privacy expectations, and we try to meet those needs even when the United States does not have the same privacy framework as other countries.</p>
+<p>We provide a high standard of privacy protection—as described in this Privacy Policy—to all our users around the world, regardless of their country of origin or location, and we are proud of the levels of notice, choice, accountability, security, data integrity, access, and recourse we provide. We work hard to comply with the applicable data privacy laws wherever we do business, working with our Data Protection Officer as part of a cross-functional team that oversees our privacy compliance efforts. </p>
+
+<h2 id="how-we-communicate-with-you">How we communicate with you</h2>
+<p>By creating a WeCarry account (by logging in) you'll automatically be enrolled in our marketing newsletter. We promise not to spam you and we'll do our best to keep the information in these emails relevant and interesting to all. You can unsubscribe from our newsletter at any time by clicking the Unsubscribe link in the email. </p>
+<p>WeCarry will send system-generated notifications whenever specific events occur in the system. You can tune and configure some of your notification preferences from your profile. </p>
+<p>We may also reach out to you personally if you've encountered an error with the system or if we'd like to ask for your participation in trying new features or giving us feedback about WeCarry. </p>
+
+<h2 id="changes-to-our-privacy-policy">Changes to our Privacy Policy</h2>
+<p>Although most changes are likely to be minor, WeCarry may change our Privacy Policy from time to time. We will provide notification to users of material changes to this Privacy Policy through our Website at least 30 days prior to the change taking effect by posting a notice on our home page or sending email to the primary email address specified in your WeCarry account.</p>
+
+<h2 id="contacting-us">Contacting us</h2>
+<p>You may contact us any time for any reason by submitting a ticket at <a href="https://wecarry.freshdesk.com/" target="_blank">https://wecarry.freshdesk.com/</a> or emailing <a href="mailto:support@wecarry.app">support@wecarry.app</a>.</p>
