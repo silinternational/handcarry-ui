f1c8e0fae44760f7e3526860726e9d21afa7ffca
Matt Henderson <matt_henderson@sil.org>
2020-01-07 16:51:58 -0500

    Add support for a smaller version of the RequestTile

--- src/components/RequestTile.svelte
+++ src/components/RequestTile.svelte
@@ -6,12 +6,17 @@ import { push } from 'svelte-spa-router'
 import { format } from 'date-fns'
 
 export let request;
+export let smaller;
 
 $: user = request.createdBy || {}
 $: size = request.size
 </script>
 
 <style>
+h3.smaller {
+  font-size: 1.25rem;
+  margin-bottom: 0.25rem;
+}
 .request-tile {
   border-color: #444444;
   cursor: pointer;
@@ -21,20 +26,26 @@ $: size = request.size
   overflow: hidden;
   height: 10rem;
 }
+.request-tile > .request-image.smaller {
+  height: 6rem;
+}
+.smaller {
+  font-size: 0.9rem;
+}
 </style>
 
 <div class="card request-tile" on:click={ () => push(`/requests/${request.id}`) }>
-  <div class="card-img-top request-image text-center">
+  <div class="card-img-top request-image text-center" class:smaller>
     <RequestImage {request} />
   </div>
   <div class="card-body p-2">
-    <h3 class="card-title">{request.title}</h3>
-    <div class="card-text">{request.destination.description}</div>
+    <h3 class="card-title" class:smaller>{request.title}</h3>
+    <div class="card-text" class:smaller>{request.destination.description}</div>
   </div>
   <div class="card-footer p-2">
     <div class="row">
       <div class="col"><UserAvatar {user} small /></div>
-      <div class="col-auto"><a class="pr-1" href="#/requests/{request.id}">View</a></div>
+      <div class="col-auto"><a class="pr-1" class:smaller href="#/requests/{request.id}">View</a></div>
     </div>
   </div>
 </div>
