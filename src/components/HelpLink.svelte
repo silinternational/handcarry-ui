0c243a5ba4bdff2240a53252409210a13fa7cc93
Billy Clark <billy_clark@sil.org>
2020-01-08 14:31:47 -0500

    users could not contact support

---
+++ src/components/HelpLink.svelte
@@ -0,0 +1,21 @@
+<script>
+const formTitle = 'Support & Feedback'
+const submitTitle = 'Submit'
+const submitThanks = 'Thank you for contacting us'
+</script>
+
+<a href="#" data-toggle="modal" data-target="#freshdesk" class={$$props.class}>
+  <slot />
+</a>
+
+<div class="modal fade" id="freshdesk" tabindex="-1" role="dialog" aria-labelledby="freshdesk" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <iframe title="Feedback Form"
+                src="https://wecarry.freshdesk.com/widgets/feedback_widget/new?widgetType=embedded&formTitle={encodeURIComponent(formTitle)}&submitTitle={encodeURIComponent(submitTitle)}&submitThanks={encodeURIComponent(submitThanks)}&screenshot=No&captcha=yes" 
+                id="freshwidget-embedded-form" class="freshwidget-embedded-form" scrolling="no" height="500px" width="100%" frameborder="0" />
+      </div>
+    </div>
+  </div>
+</div>
