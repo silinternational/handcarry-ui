7eb24b6ff792be5ac6f9d51345e28123aaadef7c
Billy Clark <billy_clark@sil.org>
2020-01-09 11:29:54 -0500

    no usage analytics

---
+++ src/data/analytics.js
@@ -0,0 +1,29 @@
+import { location } from 'svelte-spa-router'
+
+init()
+
+// https://developers.google.com/analytics/devguides/collection/analyticsjs
+function init() {
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)}
+  ga.l=+new Date
+
+  ga('create', process.env.GOOGLE_ANALYTICS_ID, 'auto')
+
+  loadLib()
+
+  location.subscribe(sendPageView)
+}
+
+function loadLib() {
+  const el = document.createElement('script')
+
+  el.src = 'https://www.google-analytics.com/analytics.js'
+  el.async = true
+  
+  document.head.appendChild(el)
+}
+
+function sendPageView(route) {
+  // https://developers.google.com/analytics/devguides/collection/analyticsjs/pages#pageview_fields
+  ga('send', 'pageview', route)
+}
