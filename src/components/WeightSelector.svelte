ced542f267deb9116c9e9d34c1512e76983e3458
Matt Henderson <matt_henderson@sil.org>
2020-02-07 11:31:46 -0500

    Fix weight selector updates

--- src/components/WeightSelector.svelte
+++ src/components/WeightSelector.svelte
@@ -9,32 +9,51 @@ const kilogramsPerPound = 0.45359237
 let pounds
 let units
 
-//$: dispatch('change', kilograms)
+// If it looks like the value was provided as pounds, default to showing the value in pounds.
+$: kilograms && wasProbablyStoredAsPounds(kilograms) && showPoundsOnInitialLoad()
 
-function roundInputValue(event) {
-  const value = event.target.value
-  if (value) {
-    /* The rounding precision here (e.g. 0.01) should match the "step" value of the input. */
-    event.target.value = Math.round(event.target.value * 100) / 100
+function wasProbablyStoredAsPounds(kilograms) {
+  return String(kilograms).indexOf('.') >= 0
+}
+
+let showPoundsOnInitialLoad = function() {
+  // We only ever want to do this once (per "page"/view load).
+  units = 'lb'
+  pounds = Math.round(kilogramsToPounds(kilograms))
+  
+  showPoundsOnInitialLoad = Function() // noop
+}
+
+function onNumberChanged(event) {
+  // If the user emptied out the field...
+  if (!event.target.value) {
+    pounds = null
+    kilograms = null
+  } else {
+    if (units === 'kg') {
+      kilograms = normalize(kilograms)
+      pounds = kilogramsToPounds(kilograms)
+    } else {
+      pounds = normalize(pounds)
+      kilograms = poundsToKilograms(pounds)
+    }
   }
-}
-
-function onKilogramsInputChanged(event) {
-  console.log('onKilogramsChanged:', event.target.value, 'kilograms:', kilograms) // TEMP
-  pounds = kilogramsToPounds(kilograms)
   dispatch('change', kilograms)
 }
 
-function onPoundsInputChanged(event) {
-  console.log('onPoundsChanged:', event.target.value, 'pounds:', pounds) // TEMP
-  kilograms = poundsToKilograms(pounds)
-  dispatch('change', kilograms)
+/**
+ * Enforce some basic requirements:
+ * - No negative values
+ * - No decimal values (round them)
+ * - Don't allow zero (0), replace it with one (1)
+ */
+function normalize(value) {
+  return Math.abs(Math.round(value)) || 1
 }
 
 function onUnitsChanged(event) {
-  console.log('onUnitsChanged:', event.target.value, 'units:', units, 'kilograms:', kilograms, 'pounds:', pounds) // TEMP
-  
   // Copy the value over to the (newly visible) input field.
+  // NOTE: `units` won't have the new value yet.
   if (event.target.value === 'kg') {
     kilograms = pounds
     pounds = kilogramsToPounds(kilograms)
@@ -47,7 +66,7 @@ function onUnitsChanged(event) {
 }
 
 function kilogramsToPounds(kgs) {
-  return kgs / kilogramsPerPound // Converting kilograms to pounds
+  return kgs / kilogramsPerPound
 }
 
 function poundsToKilograms(lbs) {
@@ -58,9 +77,11 @@ function poundsToKilograms(lbs) {
 <div class="form-row">
   <div class="col-2">
     {#if units === 'lb' }
-      lbs: <input type="number" on:change={onPoundsInputChanged} class="form-control" min="0" bind:value={pounds} />
+      <input type="number" aria-describedby="weightInputField" on:change={onNumberChanged} class="form-control" min="0"
+             bind:value={pounds} />
     {:else}
-      kgs: <input type="number" on:change={onKilogramsInputChanged} class="form-control" min="0" bind:value={kilograms} />
+      <input type="number" aria-describedby="weightInputField" on:change={onNumberChanged} class="form-control" min="0"
+             bind:value={kilograms} />
     {/if}
   </div>
   <div class="col-auto">
@@ -70,9 +91,8 @@ function poundsToKilograms(lbs) {
     </select>
   </div>
 </div>
-
-<pre>
-kilograms: { JSON.stringify(kilograms) }
-pounds: { JSON.stringify(pounds) }
-units: { JSON.stringify(units) }
-</pre>
+<div class="form-row">
+  <div class="col-auto">
+    <small id="weightInputField" class="form-text text-muted">Approximate weight in whole numbers</small>
+  </div>
+</div>
