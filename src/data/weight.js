653f72f1204ee3f36fa7514956821e12a5022ff4
Matt Henderson <matt_henderson@sil.org>
2020-02-07 11:36:02 -0500

    Fix method exports in weight.js

--- src/data/weight.js
+++ src/data/weight.js
@@ -1,9 +1,9 @@
 const kilogramsPerPound = 0.45359237
 
-function kilogramsToPounds(kgs) {
+export function kilogramsToPounds(kgs) {
   return kgs / kilogramsPerPound
 }
 
-function poundsToKilograms(lbs) {
+export function poundsToKilograms(lbs) {
   return lbs * kilogramsPerPound
 }
