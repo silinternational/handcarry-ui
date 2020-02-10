e674536783fade5929e44b2dd727a9021242fd2f
Matt Henderson <matt_henderson@sil.org>
2020-02-10 10:14:44 -0500

    Show the specified weight, even if `0`

--- src/views/Request.svelte
+++ src/views/Request.svelte
@@ -87,7 +87,7 @@ function goToConversation(conversationId) {
               <dd>{ (new Date(request.neededBefore + ' 00:00:00')).toLocaleDateString() }</dd>
             {/if}
             
-            {#if request.kilograms }
+            {#if request.kilograms != null }
               <dt>Weight</dt>
               <dd><WeightDisplay kilograms={request.kilograms} /></dd>
             {/if}
