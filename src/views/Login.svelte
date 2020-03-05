06c31e608b059283f53c1b96ffc13c70d2e2782b
Billy Clark <billy_clark@sil.org>
2020-03-05 11:51:03 -0500

    non need for reactive statement here.

--- src/views/Login.svelte
+++ src/views/Login.svelte
@@ -3,7 +3,7 @@ import Login from '../components/Login.svelte'
 import { replace } from 'svelte-spa-router'
 import { me } from '../data/user'
 
-$: $me.id && replace('/requests') // if they're already authn, no need to login again.
+$me.id && replace('/requests') // if they're already authn, no need to login again.
 </script>
 
 <Login>
