9d190510e9c31b9e2287b717e7df5aa45eae5099
Billy Clark <billy_clark@sil.org>
2020-03-13 16:50:05 -0400

    PR feedback

--- src/components/GridListToggle.svelte
+++ src/components/GridListToggle.svelte
@@ -6,15 +6,14 @@ import { createEventDispatcher } from 'svelte'
 export let choice = 'grid'
 
 const dispatch = createEventDispatcher()
-const classes = `btn btn-sm ${$$props.class}`
 
 $: isGrid = choice === 'grid'
 $: isList = choice === 'list'
 </script>
 
-<button title="Show as a grid" on:click={() => dispatch('click', 'grid')} class={classes} class:btn-secondary={isGrid} class:btn-outline-secondary={!isGrid}>
+<button title="Show as a grid" on:click={() => dispatch('click', 'grid')} class="btn btn-sm {$$props.class}" class:btn-secondary={isGrid} class:btn-outline-secondary={!isGrid}>
   <Icon icon={faTh} />
 </button>
-<button  title="Show as a list" on:click={() => dispatch('click', 'list')} class={classes} class:btn-secondary={isList} class:btn-outline-secondary={!isList}>
+<button  title="Show as a list" on:click={() => dispatch('click', 'list')} class="btn btn-sm {$$props.class}" class:btn-secondary={isList} class:btn-outline-secondary={!isList}>
   <Icon icon={faThList} />
 </button>
