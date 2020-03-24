5021096afaa70de4f4cb164cd5fdc9a16044f7c9
Schparky <3172830+Schparky@users.noreply.github.com>
2020-03-24 14:20:30 -0600

    change capitalization of labels

--- src/components/CreateWatch.svelte
+++ src/components/CreateWatch.svelte
@@ -35,7 +35,7 @@ $: watchFilters = getFiltersForWatch(filter)
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header bg-primary">
-            <h4 class="modal-title text-white">Create an Alert</h4>
+            <h4 class="modal-title text-white">Create an alert</h4>
             <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true" class="align-text-top">×</span>
             </button>
@@ -45,10 +45,10 @@ $: watchFilters = getFiltersForWatch(filter)
             <div class="modal-body">
               <div class="form-group">
                 <div>
-                  Alert Settings:
+                  Alert settings:
                 </div>
-                {#each watchFilters as f}
-                  <FilterTag label="{f.label}" hideCloseButton />
+                {#each watchFilters as watchFilter}
+                  <FilterTag label="{watchFilter.label}" hideCloseButton />
                 {/each}
               </div>
               <div class="form-group">
@@ -60,7 +60,7 @@ $: watchFilters = getFiltersForWatch(filter)
             </div>
             <div class="modal-footer justify-content-between">
               <button type="button" class="btn btn-outline-dark" data-dismiss="modal" tabindex="-1">Close</button>
-              <button type="submit" class="btn btn-primary">Create Alert</button>
+              <button type="submit" class="btn btn-primary">Create alert</button>
             </div>
           </form>
         {:else}
