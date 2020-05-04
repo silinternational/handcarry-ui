e0613de514fae723531b5597602ab3eb03b4d166
Billy Clark <billy_clark@sil.org>
2020-05-04 14:40:34 -0400

    unused code

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -8,7 +8,7 @@ $: e = $error
   <div class="alert alert-danger alert-dismissible" role="alert">
     <h4 class="alert-heading">Something went wrong</h4>
     
-    <p class="text-monospace">{@html e.message}</p>
+    <p class="text-monospace">{e.message}</p>
     
     <button on:click={reset} class="close" aria-label="Close">&times;</button>
   </div>
