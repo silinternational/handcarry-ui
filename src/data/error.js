aebc1517ecc15faf2690dc328c2b8a373c2d0174
Schparky <3172830+Schparky@users.noreply.github.com>
2021-06-09 14:39:19 -0600

    handle api error correctly by redirecting to /login with a return-to url

--- src/data/error.js
+++ src/data/error.js
@@ -13,25 +13,6 @@ function init() {
   // the incoming object here is from the browser https://developer.mozilla.org/en-US/docs/Web/API/PromiseRejectionEvent
   window.onunhandledrejection = ({reason}) => {
     // `reason` will contain whatever was thrown (which should only be from `throwError` below)
-
-    if (reason.code === 401) {
-      reason.message = '' // message details for a 401 aren't really useful to the user at this point.
-
-      // we need to get the user over to the login page but we may want to set up some additional
-      // handling in case they were in the middle of something in the app already, i.e., they
-      // already authenticated but their credenitals expired while they were looking at requests.
-      const currentRoute = window.location.pathname.substr(1)
-      let loginRoute = '/login'
-      if (! ['/', loginRoute].includes(currentRoute)) {
-        // they were on some page in the app already and lost their credentials,
-        // we'll want to give them a little info and get them back to where they were.
-        loginRoute += `?return-to=${currentRoute}`
-      }
-
-      // FIXME: this refreshes the browser, which causes this code to re-execute infinitely
-      // window.location = loginRoute
-    }
-
     error.set(reason)
 
     scrollTo(0,0) // ensures the user sees the error message at the top of the page
