f94487df94c58265c2dced141f84042bcb9f3911
captaincrazybro <myeyesareonmizar@gmail.com>
2021-07-28 09:15:39 -0400

    Fixed a rebase error

--- src/data/events.js
+++ src/data/events.js
@@ -45,32 +45,6 @@ export async function deleteOneEvent(id) {
   }
 }
 
-export async function getOneEvent(id) {
-  try {
-    loading.set(true)
-
-    return await GET(`/events/${id}`)
-  } catch (err) {
-    throw err
-  } finally {
-    loading.set(false)
-  }
-}
-
-export async function deleteOneEvent(id) {
-  try {
-    loading.set(true)
-
-    await DELETE(`/events/${id}`)
-    loadEvents()
-    return null
-  } catch (err) {
-    throw err 
-  } finally {
-    loading.set(false)
-  }
-}
-
 export async function create(event) {
   const newEvent = await POST('events', {
     name: event.name,
