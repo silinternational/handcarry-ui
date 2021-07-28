0ad358cff98099e581ddebc775cfa7bb955cde60
captaincrazybro <myeyesareonmizar@gmail.com>
2021-07-28 17:03:40 -0400

    Made it so that alerts go away when you change your window location

--- src/data/error.js
+++ src/data/error.js
@@ -29,3 +29,24 @@ export function throwError(message = '', code = 0) {
     message,
   }
 }
+
+// window change event
+history.pushState = (f => function pushState(){
+  var ret = f.apply(this, arguments);
+  window.dispatchEvent(new Event('pushstate'));
+  window.dispatchEvent(new Event('locationchange'));
+  return ret;
+})(history.pushState);
+
+history.replaceState = (f => function replaceState(){
+  var ret = f.apply(this, arguments);
+  window.dispatchEvent(new Event('replacestate'));
+  window.dispatchEvent(new Event('locationchange'));
+  return ret;
+})(history.replaceState);
+
+window.addEventListener('popstate',()=>{
+  window.dispatchEvent(new Event('locationchange'))
+});
+
+window.addEventListener("locationchange", reset)
